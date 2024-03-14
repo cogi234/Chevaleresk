bc3fde7278f9ce3b0eacd71effa34043b926e03e
cogi234 <colinbougie@gmail.com>
2024-03-14 17:35:50 -0400

    Display money and stop displaying some stuff if not connected

--- views/header.php
+++ views/header.php
@@ -2,19 +2,52 @@
 
 require_once "php/htmlUtilities.php";
 
-// Values
-$cart_amount = 3;
-$money_amount = 33;
-
 // Links
 $icon_money_url = "";
 $icon_cart_url = "";
 $icon_profile_url = "";
+$icon_inscription_url = "";
+$icon_connection_url = "";
+
+//Display only when connected
+$money_section = "";
+$cart_section = "";
+$profile_section = "";
+if (isset($_SESSION["connected"]) && $_SESSION["connected"] == true) {
+    //Money amount
+    $money_amount = select("solde", "joueurs", "idJoueur = " . $_SESSION["idJoueur"])[0];
+    $money_section = <<<HTML
+    <a id="header_money" class="header-icon fa-solid fa-money-bill" href="$icon_money_url">
+        <div>
+            <span>$money_amount</span>
+        </div>
+    </a>
+    HTML;
+
+    //Cart amount
+    $cart_amount = select("count(*)", "panier", "idJoueur = " . $_SESSION["idJoueur"])[0];   
+    $cart_section = <<<HTML
+    <a id="header_cart" class="header-icon fa-solid fa-cart-shopping" href="$icon_cart_url">
+        <span>$cart_amount</span>
+    </a>
+    HTML;
+
+    //Profile
+    $profile_section = <<<HTML
+    <a id="header_profile" class="header-icon fa-solid fa-user" href="$icon_profile_url"></a>
+    HTML;
+} else {
+    //Profile
+    $profile_section = <<<HTML
+    <a id="header_profile" class="header-icon fa-solid fa-user-plus" href="$icon_inscription_url" title="Inscription"></a>
+    <a id="header_profile" class="header-icon fa-solid fa-user" href="$icon_connection_url" title="Connexion"></a>
+    HTML;
+}
 
 // Dropdown
 $dropdown = dropdown("", [
-    dropdown_item("Lorem Upon 1", "#"),
-    dropdown_item("Lorem Upon 2", "#"),
+    dropdown_item("Enigma", "#"),
+    dropdown_item("Panoramix", "#"),
 ], "fa-solid fa-angle-down header_dropdown");
 
 // Content
@@ -38,18 +71,10 @@ $header_content = <<<HTML
      <!-- RIGHT  -->
      <div id="header-section-right" class="header-section">
         <!-- MONEY -->
-        <a id="header_money" class="header-icon fa-solid fa-money-bill" href="$icon_money_url" target="blank">
-            <div>
-                <span>$money_amount</span>
-            </div>
-        </a>
-
+        $money_section
         <!-- CART -->
-        <a id="header_cart" class="header-icon fa-solid fa-cart-shopping" href="$icon_cart_url" target="blank">
-            <span>$cart_amount</span>
-        </a>
-        
+        $cart_section
         <!-- PROFILE -->
-        <a id="header_profile" class="header-icon fa-solid fa-user" href="$icon_profile_url" target="blank"></a>
+        $profile_section
      </div>
 HTML;
