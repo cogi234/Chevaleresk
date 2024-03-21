e0902d62602056297aa8dadc17927f1b27a63eb4
WarperSan <leumas.ecole@gmail.com>
2024-03-21 12:43:05 -0400

    Merge

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
 <?php
 
-require_once "php/phpUtilities.php";
-require_once "php/storeHTML.php";
-
+// PDO
 require_once "php/pdo.php";
 require_once "php/items.php";
 
+// UTILITIES
+require_once "php/phpUtilities.php";
+require_once "php/pdoUtilities.php";
+
+// HTML
+require_once "php/storeHTML.php";
+require_once "php/filterHTML.php";
+
 $is_admin = false;
+
+// Pagination
+$show_count = 18;
+$page_count = 0;
+
+// Sort
+$sort_types = Item::TYPES;
+
+// Select
+$condition = in(Item::TYPE, ...$sort_types);
+
+if (!$is_admin)
+    $condition = _and($condition, equals(Item::SELLABLE, 1));
+
+$other = combine(
+    orderByAll([Item::PRICE], [Item::NAME]),
+    limit($show_count, $page_count * $show_count)
+);
 
 $items = Item::selectAll(
     [
@@ -17,7 +41,8 @@ $items = Item::selectAll(
         Item::IMAGE,
         Item::TYPE
     ],
-    Item::SELLABLE . ' = 1'
+    $condition,
+    $other
 );
 
 // Items
@@ -31,7 +56,7 @@ for ($i = 0; $i < count($items); $i++) {
     $item_name = $item->Nom;
     $item_price = $item->Prix;
     $item_quantity = $item->Quantite;
-    $item_image = $item->Image;
+    $item_image = $item->getImage();
     $item_icon = $item->getIcon();
 
     // Render
@@ -45,10 +70,27 @@ for ($i = 0; $i < count($items); $i++) {
     );
 }
 
+for ($i = 0; $i < $show_count - count($items); $i++)
+    $items_html .= "<i></i>";
+
+// Filters
+$filter_html = filter_render(<<<HTML
+    <p>Filtres</p>
+    <hr>
+HTML);
+
 $body_content = <<<HTML
-    <div style="width: 100%;">
-        <div class="store-item-holder">
-            $items_html
+    $filter_html
+
+    <div style="display: flex;">
+        <!-- ITEMS -->
+        <div id="store-item-parent">
+            <div class="store-item-holder">
+                $items_html
+            </div>
+
+            <!-- PAGES -->
+            <div></div>
         </div>
     </div>
 HTML;
