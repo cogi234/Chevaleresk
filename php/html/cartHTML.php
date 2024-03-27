083157a1b5e2bc020608db20ce02f87ccababf76
cogi234 <colinbougie@gmail.com>
2024-03-27 10:36:59 -0400

    html cleanup

--- php/html/cartHTML.php
+++ php/html/cartHTML.php
@@ -10,30 +10,30 @@ const PATH_IMAGES = "images/items/images/";
  */
 function cartItem(
     string $image,
-    string $nom,
-    int $quantite,
-    int $quantiteStock,
+    string $name,
+    int $quantity,
+    int $quantityStock,
     int $idItem,
 ): string{
     $content = "";
     $image = PATH_IMAGES . $image;
         //check if the item is still in stock
-        if($quantiteStock > 0){
+        if($quantityStock > 0){
             $content .= <<<HTML
             <div class="cart-item">
                 <a href="#" class="cart-item-image"><img src="$image"/></a>
                     <div class="cart-item-info">
-                        <p class="name-item">$nom</p>
+                        <p class="name-item">$name</p>
                         <div class="number-item">
                             <p>x</p>
-                            <p class="cart-quantity">$quantite</p>
-                            <a href="cartAdd.php?id=$idItem" class="fa fa-plus cart-quantity-modifier"></a>
-                            <a href="cartRemove?id=$idItem" class="fa fa-minus cart-quantity-modifier"></a>
+                            <p class="cart-quantity">$quantity</p>
+                            <a href="operations/cartAdd.php?id=$idItem" class="fa fa-plus cart-quantity-modifier"></a>
+                            <a href="operations/cartRemove?id=$idItem" class="fa fa-minus cart-quantity-modifier"></a>
                         </div>
                     </div>
                     <div class="cart-item-remove-error">
                     <p hidden class="item-errorMessage" color="red">Hors Stock...</p>
-                    <a class="remove-item fa fa-xmark" href="cartRemove.php?id=$idItem&quantity=$quantite"></a>
+                    <a class="remove-item fa fa-xmark" href="operations/cartRemove.php?id=$idItem&quantity=$quantity"></a>
             HTML;   
         }else{
             //if false show message
@@ -41,17 +41,17 @@ function cartItem(
                 <div class="cart-item-outofstock">
                 <div class="cart-item-image"><img src="$image"/></div>
                     <div class="cart-item-info">
-                        <p class="name-item">$nom</p>
+                        <p class="name-item">$name</p>
                         <div class="number-item">
                             <p>x</p>
-                            <p class="cart-quantity">$quantite</p>
-                            <a href="cartAdd.php?id=$idItem" class="fa fa-plus cart-quantity-modifier"></a>
-                            <a href="cartRemove?id=$idItem" class="fa fa-minus cart-quantity-modifier"></a>
+                            <p class="cart-quantity">$quantity</p>
+                            <a href="operations/cartAdd.php?id=$idItem" class="fa fa-plus cart-quantity-modifier"></a>
+                            <a href="operations/cartRemove?id=$idItem" class="fa fa-minus cart-quantity-modifier"></a>
                         </div>
                     </div>
                     <div class="cart-item-remove-error">
                     <p class="item-errorMessage" color="red">Hors Stock...</p>
-                    <a class="remove-item fa fa-xmark" href="cartRemove.php?id=$idItem&quantity=$quantite"></a>
+                    <a class="remove-item fa fa-xmark" href="operations/cartRemove.php?id=$idItem&quantity=$quantity"></a>
             HTML;
         }
         $content .= <<<HTML
