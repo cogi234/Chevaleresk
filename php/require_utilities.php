a50876e40469e9972c20e14dff52a266616dbf35
WarperSan <leumas.ecole@gmail.com>
2024-03-27 11:37:56 -0400

    Require Documentation

--- php/require_utilities.php
+++ php/require_utilities.php
@@ -1,5 +1,12 @@
 <?php
 
+// 
+// Every file that is related to an AJAX request needs to use require like this:
+// require_once dirname(__FILE__, X) . "/require_utilities.php";
+// require_path("php/...");
+//
+// The X is the number of level the file is from the folder php
+
 function require_path(string $path): void
 {
     require_once dirname(__FILE__, 2) . "/" . $path;
