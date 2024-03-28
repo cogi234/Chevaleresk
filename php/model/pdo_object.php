0018829bfa43a4c2957958a9b75408179885d9b5
cogi234 <colinbougie@gmail.com>
2024-03-28 13:39:24 -0400

    pdo_object useful funtions (#29)
    * Item type models
    
    * Added get_columns and complete selects

--- php/model/pdo_object.php
+++ php/model/pdo_object.php
@@ -63,6 +63,41 @@ abstract class PDO_Object
     }
 
     /**
+     * Returns a list of every database column
+     * @author Colin Bougie
+     * Date of creation    : 2024/03/28
+     * Date of modification: 2024/03/28
+     */
+    public static function get_columns(): array
+    {
+        $columns = [];
+        // Get properties
+        $class = new ReflectionClass(get_called_class());
+
+        foreach ($class->getProperties() as $key => $prop) {
+            $attributes = $prop->getAttributes(PDO_Object_Id::class);
+            
+            //We check if the property is a PDO_Object
+            $is_pdo_object = is_subclass_of($prop->class, get_called_class());
+            //If so, we get its columns to add to ours
+            if ($is_pdo_object){
+                $child_columns = $prop->class::get_columns();
+                $columns = array_merge($columns, $child_columns);
+            }
+
+            //If there isn't an attribute, there's no columns name and we skip
+            if (count($attributes) < 1)
+                continue;
+
+            // Get infos
+            $identifier = $attributes[0]->getArguments()[0];
+
+            array_push($columns, $identifier);
+        }
+        return $columns;
+    }
+
+    /**
      * Called when this item is initializes. Useful to initialize more complexe structures
      * @author @WarperSan
      * Date of creation    : 2024/03/27
@@ -104,6 +139,20 @@ abstract class PDO_Object
         }, $items);
     }
 
+    /**
+     * Fetches every result of the given select request with the given parameters and all the columns
+     * @author Colin Bougie
+     * Date of creation    : 2024/03/28
+     * Date of modification: 2024/03/28
+     */
+    public static function selectAllComplete(
+        string $condition = "",
+        string $other = ""
+    ): array {
+        $selectors = static::get_columns();
+        return static::selectAll($selectors, $condition, $other);
+    }
+
     public static function select(
         array $selectors,
         string $condition = "",
@@ -120,6 +169,20 @@ abstract class PDO_Object
             return false;
 
         return static::create_self($item);
+    }
+
+    /**
+     * Fetches every result of the given select request with the given parameters and all the columns
+     * @author Colin Bougie
+     * Date of creation    : 2024/03/28
+     * Date of modification: 2024/03/28
+     */
+    public static function selectComplete(
+        string $condition = "",
+        string $other = ""
+    ): PDO_Object|bool {
+        $selectors = static::get_columns();
+        return static::select($selectors, $condition, $other);
     }
 
     /**
