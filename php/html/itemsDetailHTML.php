611f7a6b4853d1b6337c19f0b1b48426f3bf6682
WarperSan <leumas.ecole@gmail.com>
2024-04-02 15:15:24 -0400

    Current HTML

--- php/html/itemsDetailHTML.php
+++ php/html/itemsDetailHTML.php
@@ -1,11 +1,15 @@
 <?php
 
-$image_url = "TEMP";
-$icon_url = "TEMP";
+// Styles
+isset_default($styles_view);
+$styles_view .= '<link rel="stylesheet" href="css/details_items_styles.css">';
 
-$name = "TEMP";
-$description = "TEMP";
-$price = "TEMP";
+$image_url = "images/items/images/default_armor.png";
+$icon_url = "images/items/icons/gun.svg";
+
+$name = "Ail";
+$description = "Une épée courte pour les aventuriers débutants.";
+$price = "3";
 
 $type_html = "TEMP HTML";
 $stock_html = "TEMP HTML";
@@ -13,41 +17,43 @@ $quantity_html = "TEMP HTML";
 $buy_html = "TEMP HTML";
 
 $details_content = <<<HTML
-    <div>
-        <!-- IMAGE -->
-        <div>
-            <img src="$image_url" >
-        </div>
-
-        <!-- DETAILS -->
-        <div>
-            <!-- TITLE -->
+    <div id="details-container">
+        <div id="details">
+            <!-- IMAGE -->
             <div>
-                <img src="$icon_url">
-                <p>$name</p>
+                <img id="details-image" src="$image_url" >
             </div>
 
-            <!-- EVALUTION -->
-            <div></div>
-            
-            <!-- TYPE DETAILS -->
-            <div>
-                $type_html
+            <!-- DETAILS -->
+            <div id="details-details">
+                <!-- TITLE -->
+                <div id="details-title">
+                    <i id="details-type-icon" style="mask-image: url('$icon_url');"></i>
+                    <p id="details-name">$name</p>
+                </div>
+
+                <!-- EVALUTION -->
+                <div></div>
+                
+                <!-- TYPE DETAILS -->
+                <div>
+                    $type_html
+                </div>
             </div>
         </div>
-    </div>
 
-    <!-- DESCRIPTION -->
-    <div>
-        $description
-    </div>
+        <!-- DESCRIPTION -->
+        <div id="details-description">
+            $description
+        </div>
 
-    <!-- REVIEWS -->
-    <div></div>
+        <!-- REVIEWS -->
+        <div></div>
+    </div>
 
     <!-- PANIER -->
-    <div>
-        $price
+    <div id="details-cart">
+        $ $price
         $stock_html
         $quantity_html
         $buy_html
