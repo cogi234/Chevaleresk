ff6e48e212a96f649aebdf01ae0d9bb878077b6f
cogi234 <colinbougie@gmail.com>
2024-04-02 14:25:30 -0400

    Done

--- php/html/cartHTML.php
+++ php/html/cartHTML.php
@@ -15,47 +15,65 @@ function cartItem(
     int $quantityStock,
     int $idItem,
 ): string {
-    $content = "";
+    isset_default($content);
+    
+    $elementId = "cart-item-$idItem";
+
     //check if the item is still in stock
-    if ($quantityStock > 0) {
+    if ($quantityStock >= $quantity) {
         $content .= <<<HTML
-            <div class="cart-item">
+            <div class="cart-item" id="$elementId">
                 <a href="#" class="cart-item-image"><img src="$image"/></a>
                     <div class="cart-item-info">
                         <p class="name-item">$name</p>
                         <div class="number-item">
-                            <p>x</p>
+                            <div class="fa fa-minus cart-quantity-modifier"
+                                hx-post="operations/cartRemove.php?id=$idItem&partial=1"
+                                hx-trigger="click"
+                                hx-target="#$elementId"
+                                hx-swap="outerHTML"></div>
                             <p class="cart-quantity">$quantity</p>
-                            <a href="operations/cartAdd.php?id=$idItem" class="fa fa-plus cart-quantity-modifier"></a>
-                            <a href="operations/cartRemove?id=$idItem" class="fa fa-minus cart-quantity-modifier"></a>
+                            <div class="fa fa-plus cart-quantity-modifier"
+                                hx-post="operations/cartAdd.php?id=$idItem&partial=1"
+                                hx-trigger="click"
+                                hx-target="#$elementId"
+                                hx-swap="outerHTML"></div>
                         </div>
                     </div>
                     <div class="cart-item-remove-error">
                     <p hidden class="item-errorMessage" color="red">Hors Stock...</p>
                     <a class="remove-item fa fa-xmark" href="operations/cartRemove.php?id=$idItem&quantity=$quantity"></a>
+                </div>
+            </div>
             HTML;
     } else {
         //if false show message
         $content .= <<<HTML
-                <div class="cart-item-outofstock">
+            <div class="cart-item-outofstock" id="$elementId">
                 <div class="cart-item-image"><img src="$image"/></div>
                     <div class="cart-item-info">
                         <p class="name-item">$name</p>
                         <div class="number-item">
-                            <p>x</p>
+                            <div class="fa fa-minus cart-quantity-modifier"
+                                hx-post="operations/cartRemove.php?id=$idItem&partial=1"
+                                hx-trigger="click"
+                                hx-target="#$elementId"
+                                hx-swap="outerHTML"></div>
                             <p class="cart-quantity">$quantity</p>
-                            <a href="operations/cartAdd.php?id=$idItem" class="fa fa-plus cart-quantity-modifier"></a>
-                            <a href="operations/cartRemove?id=$idItem" class="fa fa-minus cart-quantity-modifier"></a>
+                            <div class="fa fa-plus cart-quantity-modifier"
+                                hx-post="operations/cartAdd.php?id=$idItem&partial=1"
+                                hx-trigger="click"
+                                hx-target="#$elementId"
+                                hx-swap="outerHTML"></div>
                         </div>
                     </div>
                     <div class="cart-item-remove-error">
                     <p class="item-errorMessage" color="red">Hors Stock...</p>
                     <a class="remove-item fa fa-xmark" href="operations/cartRemove.php?id=$idItem&quantity=$quantity"></a>
-            HTML;
-    }
-    $content .= <<<HTML
                 </div>
             </div>
-        HTML;
+            HTML;
+    }
+    
     return $content;
 }
