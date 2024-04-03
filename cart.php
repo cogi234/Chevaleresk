e36632b7a0b9ffded4cbc7aa32f12e02a0fd2e56
cogi234 <colinbougie@gmail.com>
2024-04-03 10:48:53 -0400

    Update cart.php

--- cart.php
+++ cart.php
@@ -90,7 +90,7 @@ $body_content = <<<HTML
         </div>
         <div class="cart-button">
             <button type="submit" class='cart-submit-button' $cartSubmitState>Acheter</button>
-            <button type="button" class='cart-remove-all-button' onclick="location.href='cartRemoveAll.php'">Tout retirer</button>
+            <button type="button" class='cart-remove-all-button' onclick="location.href='operations/cartRemoveAll.php'">Tout retirer</button>
         </div>
     </div>
 </form>
