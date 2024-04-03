075f67e457c4c05acd250544c8c0c3ea0604019c
cogi234 <colinbougie@gmail.com>
2024-04-02 21:26:00 -0400

    Get item data for details

--- php/html/itemsDetailHTML.php
+++ php/html/itemsDetailHTML.php
@@ -1,20 +1,84 @@
 <?php
 
+require_once "php/model/player.php";
+require_once "php/model/item.php";
+require_once "php/model/inventory_item.php";
+require_once "php/model/armor.php";
+require_once "php/model/ingredient.php";
+require_once "php/model/potion.php";
+require_once "php/model/ingredient.php";
+require_once "php/pdo/pdo_utilities.php";
+
 // Styles
 isset_default($styles_view);
 $styles_view .= '<link rel="stylesheet" href="css/details_items_styles.css">';
 
-$image_url = "images/items/images/default_armor.png";
-$icon_url = "images/items/icons/gun.svg";
+$item_id = $_GET[TAG_ID];
+$item = Item::selectComplete(equals(Item::ID, $item_id));
 
-$name = "Ail";
-$description = "Une épée courte pour les aventuriers débutants.";
-$price = "3";
 
-$type_html = "TEMP HTML";
-$stock_html = "TEMP HTML";
-$quantity_html = "TEMP HTML";
-$buy_html = "TEMP HTML";
+$image_url = $item->getImage();
+$icon_url = $item->getIcon();
+
+$name = $item->Name;
+$description = $item->Description;
+$price = $item->Price;
+$stock = $item->Quantity;
+$type = $item->Type;
+
+switch ($type) {
+    case Item::TYPES[0]:
+        $weapon = Weapon::selectComplete(equals(Weapon::ID, $item_id));
+        $efficacy = $weapon->Efficacy;
+        $weapon_type = $weapon->Type;
+        $type_html = <<<HTML
+        <div>TEMP</div>
+HTML;
+        break;
+    case Item::TYPES[1]:
+        $armor = Armor::selectComplete(equals(Weapon::ID, $item_id));
+        $material = $armor->Material;
+        $size = $armor->Size;
+        $type_html = <<<HTML
+        <div>TEMP</div>
+HTML;
+        break;
+    case Item::TYPES[2]:
+        $ingredient = Ingredient::selectComplete(equals(Weapon::ID, $item_id));
+        $ingredient_type = $ingredient->Type;
+        $rarity = $ingredient->Rarity;
+        $danger = $ingredient->Danger;
+        $type_html = <<<HTML
+        <div>TEMP</div>
+HTML;
+        break;
+    case Item::TYPES[3]:
+        $potion = Potion::selectComplete(equals(Weapon::ID, $item_id));
+        $potion_type = $potion->Type;
+        $effect = $potion->Effect;
+        $duration = $potion->Duration;
+        $type_html = <<<HTML
+        <div>TEMP</div>
+HTML;
+        break;
+}
+isset_default($type_html, "<div>Unhandled item type!</div>");
+
+$stock_html = <<<HTML
+    <div>$stock</div>
+HTML;
+
+if (is_connected()) {
+    $player_id = Player::getLocalPlayer()->Id;
+    $quantity_to_buy_html = <<<HTML
+    <div>TEMP</div>
+HTML;
+    $buy_html = <<<HTML
+    <div>TEMP</div>
+HTML;
+}
+isset_default($quantity_to_buy_html);
+isset_default($buy_html);
 
 $details_content = <<<HTML
     <div id="details-container">
@@ -55,7 +119,7 @@ $details_content = <<<HTML
     <div id="details-cart">
         $ $price
         $stock_html
-        $quantity_html
+        $quantity_to_buy_html
         $buy_html
     </div>
 HTML;
