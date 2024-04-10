cf60bec0d52b042bbd5e34547ed1733dc2fa22ff
WarperSan <leumas.ecole@gmail.com>
2024-04-10 12:25:36 -0400

    Create getLocalInformations.php

---
+++ operations/getLocalInformations.php
@@ -0,0 +1,11 @@
+<?php
+
+// PDO
+require_once "../php/model/player.php";
+
+$name = Player::getLocalPlayer()->get_fullname();
+
+echo json_encode(<<<HTML
+    <a id="header_disconnect" class="header-icon fa-solid fa-arrow-right-from-bracket" href="operations/disconnect.php" title="Se déconnecter"></a>
+    <span>Bonjour, $name!</span>
+HTML);
