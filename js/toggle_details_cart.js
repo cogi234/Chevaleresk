2d1e9c774778fa39ed43a9168ab0dea3a528fe8a
WarperSan <leumas.ecole@gmail.com>
2024-04-10 12:25:29 -0400

    Moved Scripts

--- js/toggle_details_cart.js
+++
@@ -1,5 +0,0 @@
-$("#details-cart-collapse").on("click", function () {
-    $("#details-cart").toggleClass("details-fixed-cart");
-    $(this).toggleClass("fa-plus");
-    $(this).toggleClass("fa-minus");
-});
