a16735336e0f1cb87d9dc42e3b7dcbfb3c42706c
cogi234 <colinbougie@gmail.com>
2024-04-11 12:37:19 -0400

    Rename enigma model to quest

--- php/model/quest.php
+++ php/model/quest.php
@@ -1,27 +1,36 @@
 <?php
 require_once "php/php_utilities.php";
 require_once "php/model/pdo_object.php";
+require_once "php/model/answer.php";
 
-class Quest extends PDO_Object{
-    protected const TABLE = "";
+class Quest extends PDO_Object
+{
+    protected function on_create_self(array $data): void
+    {
+        $this->Answers = Answer::selectAllComplete(equals(Answer::IDENIGMA, $this->Id));
+    }
 
+    #region PDO_Object
+    protected const TABLE = "enigmes";
+    #endregion
+
+    #region Properties
     public const ID = "idEnigme";
     #[PDO_Object_Id(Quest::ID)]
     public int $Id = -1;
-
+    
     public const TITLE = "titre";
-    #[PDO_Object_Id(Quest::TITLE)]
+    #[PDO_Object_Id(Quest::MATERIAL)]
     public string $Title = "";
-
-    public const DESCRIPTION = "question";
-    #[PDO_Object_Id(Quest::DESCRIPTION)]
-    public string $Description = "";
-
-    public const DIFFICULTY = "difficulte";
-    #[PDO_Object_Id(Quest::DIFFICULTY)]
-    public int $Difficulty = -1;
+    
+    public const QUESTION = "question";
+    #[PDO_Object_Id(Quest::SIZE)]
+    public string $Question = "";
 
     public const ALCHEMY = "alchimie";
     #[PDO_Object_Id(Quest::ALCHEMY)]
-    public int $Alchemy = -1;
+    public bool $Alchemy = false;
+
+    public array $Answers = [];
+    #endregion
 }
