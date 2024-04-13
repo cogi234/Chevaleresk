683bedae18646a3a4bdb55c255cd0938726bcbd8
FélixCrevierBéchard <filbechard04@gmail.com>
2024-04-12 20:42:41 -0400

    Update answerQuest.php

--- operations/answerQuest.php
+++ operations/answerQuest.php
@@ -1,7 +1,5 @@
 <?php
 
-use LDAP\Result;
-
 require_once "../php/php_utilities.php";
 require_once "../php/model/answer.php";
 require_once "../php/pdo/pdo_utilities.php";
@@ -13,7 +11,7 @@ if($_POST["id"]){
     $id = $_POST["id"];
     isset_default($condition);
     $condition .= equals(Answer::ID, $id);
-    $answer = Answer::select([Answer::CORRECT], $condition);
-    echo Result($answer->Respond()[0][0] == 1);
+    $answer = Answer::selectComplete($condition);
+    echo var_dump($answer->Respond());
 }
 
