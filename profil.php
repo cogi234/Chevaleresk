a4e473d07175067528fb5f9e08868a2697e2b52d
lolo2178 <lorickdenis@gmail.com>
2024-04-16 15:08:05 -0400

    ProfilComplet

--- profil.php
+++ profil.php
@@ -14,6 +14,9 @@ require_once "php/html/inventoryHTML.php";
 require_once "php/session_manager.php";
 userAccess();
 
+isset_default($styles_view);
+$styles_view .= "<link rel='stylesheet' href='css/profile_styles.css'>";
+
 $page_title = "Profil";
 
 $idPlayer = Player::getLocalPlayer()->Id;
@@ -32,24 +35,67 @@ $player_potion = $player->PotionMadeNumber;
 $player_EcuGain = $player->EcuObtainedNumber;
 $player_EcuSpent = $player->EcuSpentNumber;
 
+if ($player_alchemy <1)
+{
+    $textAlchemist = "Pas alchimiste. Devenez alchimiste en effectuant des quêtes d'alchimie dans Enigma.";
+}
 
+switch ($player_alchemy) {
+    case 1:
+        $textAlchemist = "Alchimiste débutant";
+        $statsAlchemist = <<<HTML
+        <p>Nombre de quêtes d'alchimie réussies : $player_alchemyQuest</p>
+        <p>Nombre de potions créés : $player_potion </p>
+        HTML;
+        break;
+    case 2:
+        $textAlchemist = "Alchimiste intermédiaire";
+        $statsAlchemist = <<<HTML
+        <p>Nombre de quêtes d'alchimie réussies : $player_alchemyQuest</p>
+        <p>Nombre de potions créés : $player_potion </p>
+        HTML;
+        break;
+    case 3 :
+        $textAlchemist = "Alchimiste avancé";
+        $statsAlchemist = <<<HTML
+        <p>Nombre de quêtes d'alchimie réussies : $player_alchemyQuest</p>
+        <p>Nombre de potions créés : $player_potion </p>
+        HTML;
+        break;
+    
+    default:
+        $textAlchemist = "Vous n'êtes pas alchimiste. Devenez alchimiste en effectuant des quêtes d'alchimie dans Enigma.";
+        $statsAlchemist ='';
+        break;
+}
 
 $body_content = <<<HTML
 
-    <div>
-    $player_alias
-    $player_lastName
-    $player_firstName 
-    <img id="profilePic"  src='$player_avatar' title="C'est vous!"/>
-    $player_balance 
-    $player_alchemy 
-    $player_questSuccess 
-    $player_questFailed 
-    $player_alchemyQuest
-    $player_potion 
-    $player_EcuGain 
-    $player_EcuSpent 
+    <div class='profile-container'>
+        <div>
+    <img class="header-icon" id="profile-pic"  src='$player_avatar' title="C'est vous!"/><br>
+    <div id='profile-names'> 
+        <p>$player_alias<br>
+        $player_firstName $player_lastName
+        </p>
     </div>
+    
+    </div>
+    <div id='profile-details'>
+        <div>
+            <p>$player_balance écus</p>
+            <p>$textAlchemist </p> 
+        </div>
+    <div> 
+        <p>Nombre de quêtes réussies : $player_questSuccess</p> 
+        <p>Nombre de quêtes ratés : $player_questFailed </p>
+        $statsAlchemist
+        <p>Nombre d'écus accumulés : $player_EcuGain </p>
+        <p>Nombre d'écus dépensés :$player_EcuSpent </p>
+    </div>
+    
+    </div>
+</div>
 
 HTML;
 
