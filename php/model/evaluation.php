7cf6f03427d0269bccbe4b2da6e0560c37b12c24
cogi234 <colinbougie@gmail.com>
2024-05-01 08:52:39 -0400

    evaluation model

---
+++ php/model/evaluation.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once dirname(__FILE__, 2) . "/require_utilities.php";
+require_path("php/php_utilities.php");
+require_path("php/pdo/pdo_utilities.php");
+require_path("php/model/pdo_object.php");
+require_path("php/model/inventory_item.php");
+
+class Evaluation extends PDO_Object
+{
+    #region PDO_Object
+    protected const TABLE = "commentaires";
+    #endregion
+
+    #region Properties
+    public const PLAYERID = "idJoueur";
+    #[PDO_Object_Id(Evaluation::PLAYERID)]
+    public int $PlayerId = -1;
+    
+    public const ITEMID = "idItem";
+    #[PDO_Object_Id(Evaluation::ITEMID)]
+    public int $ItemId = -1;
+    
+    public const STARS = "nbEtoiles";
+    #[PDO_Object_Id(Evaluation::STARS)]
+    public int $Stars = -1;
+    
+    public const COMMENT = "commentaire";
+    #[PDO_Object_Id(Evaluation::COMMENT)]
+    public int $Comment;
+    #endregion
+
+    #region Functions
+
+    /**
+     * @author Colin Bougie
+     * Date of creation    : 2024/05/01
+     * Date of modification: 2024/05/01
+     */
+    public function createEvaluation(int $itemId, int $stars, string $comment) : void
+    {
+        callProcedure("ajouterCommentaire", Player::getLocalPlayer()->Id, $itemId, $stars, $comment);
+    }
+    
+    /**
+     * @author Colin Bougie
+     * Date of creation    : 2024/05/01
+     * Date of modification: 2024/05/01
+     */
+    public function removeEvaluation(int $itemId) : void
+    {
+        callProcedure("retirerCommentaire", Player::getLocalPlayer()->Id, $itemId);
+    }
+
+    #endregion
+}
