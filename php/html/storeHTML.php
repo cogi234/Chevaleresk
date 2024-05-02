d04767b44e9c807230b0c1ab35dee5193f31280d
WarperSan <78861227+WarperSan@users.noreply.github.com>
2024-05-02 14:42:46 -0400

    Fix Image Styles

--- php/html/storeHTML.php
+++ php/html/storeHTML.php
@@ -44,7 +44,7 @@ HTML;
     return <<<HTML
         <a class="store-item" href="$url" title="Voir les détails de $name">
             <!-- IMAGE -->
-            <img class="store-item-image" src="$image">
+            <div class="store-item-image" style="background-image: url('$image');"></div>
 
             <!-- INFO -->
             <div class="store-item-info"> 
