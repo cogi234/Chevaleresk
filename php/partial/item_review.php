16844fb0fd3fa1f338df0e1431d807e448c7af9f
cogi234 <colinbougie@gmail.com>
2024-05-14 14:23:33 -0400

    review removal

--- php/partial/item_review.php
+++ php/partial/item_review.php
@@ -10,12 +10,24 @@ require_once "../pdo/pdo_utilities.php";
 // HTML
 require_once "../html/itemsReviewHTML.php";
 
+$local_player_id = 0;
+$is_admin = false;
+if (is_connected()){
+    $local_player_id = Player::getLocalPlayer()->Id;
+    $is_admin = Player::getLocalPlayer()->IsAdmin;
+}
+
 $evaluations = Review::selectAllComplete(
-    equals(Review::ITEMID, $_SESSION["CHECKED_ID"]),
+    _and(
+        equals(Review::ITEMID, $_SESSION["CHECKED_ID"]),
+        unequals(Review::PLAYERID, $local_player_id)
+    ),
     orderBy(Review::DATE, false)
 );
 $html = "";
+
+
 foreach ($evaluations as $key => $value)
-    $html .= show_review($value);
+    $html .= show_review($value, $is_admin);
 
 echo $html;
